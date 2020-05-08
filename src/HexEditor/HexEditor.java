a90ccf26fc88d636d101829ccb4f55e99c6bd431
Shubham Roy <sr2thehell@gmail.com>
2020-05-08 10:54:34 +0530

    Added the task 1 and task 2 with main for check

---
+++ src/HexEditor/HexEditor.java
@@ -0,0 +1,39 @@
+package HexEditor;
+import java.lang.*;
+import java.text.DecimalFormat; 
+
+public class HexEditor {
+	public static void repeat(String str, int count) {
+		if(count == 1) {
+			System.out.printf(str);
+		}
+		else {
+			System.out.printf(str);
+			repeat(str,--count);
+		}
+	}
+	public static void main (String args[]) throws Exception {
+		repeat("hello",3);
+		System.out.println();
+		formatSize(2147483647);
+		System.out.println(formatSize(2147483647));		
+		System.out.println(formatSize(123));
+		System.out.println(formatSize(83647));
+		long x = Long.parseLong("188900977659375");
+		System.out.println(formatSize(x));
+		long y = Long.parseLong("9585631");
+		System.out.println(formatSize(y));
+	}
+	public static String formatSize(long sizeInBytes) {
+		String rep[] = {"bytes","KiB","MiB","GiB","TiB","PiB","EiB"};
+		double exp = Math.log10((double)sizeInBytes)/Math.log10(2.0);
+		int power = (int)Math.floor(exp/10);
+		if (power == 0) {
+			String Finalnum = sizeInBytes+" "+rep[power];
+			return Finalnum;
+		}
+		float newnum = (float)(Math.round(Math.pow(2,exp - (10.0*power)) * 100) / 100.0);
+		String Finalnum = String.format("%.02f",newnum)+" "+rep[power];
+		return Finalnum;
+	}
+}
