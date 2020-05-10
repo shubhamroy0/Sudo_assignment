252ab34130410dc6cd91eec914d1fc4c0d123ce0
Shubham Roy <sr2thehell@gmail.com>
2020-05-10 08:06:07 +0530

    Task 4 done !

--- src/HexEditor/MyProgram.java
+++ src/HexEditor/MyProgram.java
@@ -1,10 +1,12 @@
 package HexEditor;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MyProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		if(args.length != 1) {
 			System.out.printf("You have entered wrong number of arguments. Please enter a file name like 'filename.txt'");
 			return;
@@ -12,7 +14,15 @@ public class MyProgram {
 		if (new File(args[0]).exists()) {
 			System.out.printf("The file exists");
 		} else {
-			System.out.printf("The file osn't exists");
+			System.out.printf("The file %S dosn't exists. Would you like to create it ? (yes/no)",args[0]);
+			String input = new Scanner(System.in).nextLine();
+			if (input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y")) {
+				boolean fileCreated = new File(args[0]).createNewFile();
+				if (fileCreated) 
+					System.out.printf("File created !");
+				else
+					System.out.printf("File creation failed !");
+			}
 		}
 	}
 
